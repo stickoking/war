135edcf40db0ba4a68313d2aecffd2a24e296574
Prasshant Shanmugalingam <stickoking@hotmail.com>
2022-10-31 15:06:12 +0800

    Initial Commit

---
+++ war/warApp.swift
@@ -0,0 +1,17 @@
+//
+//  warApp.swift
+//  war
+//
+//  Created by Prasshant Shanmugalingam on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct warApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
